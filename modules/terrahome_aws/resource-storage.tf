7b7e43c0f9ab4ae8fea5f851bb84b0193cacb716
Daniel Grubbs <djgrubbs@gmail.com>
2023-10-06 13:07:22 +0000

    #49 fixed issues with provider, setup two diferent homes

--- modules/terrahome_aws/resource-storage.tf
+++ modules/terrahome_aws/resource-storage.tf
@@ -26,10 +26,10 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath
+  source = "${var.public_path}/index.html"
   content_type = "text/html"
 
-  etag = filemd5(var.index_html_filepath)
+  etag = filemd5("${var.public_path}/index.html")
   lifecycle {
     replace_triggered_by = [ terraform_data.content_version.output ]
     ignore_changes = [ etag ]
@@ -37,29 +37,28 @@ resource "aws_s3_object" "index_html" {
 }
 
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
+  for_each = fileset("${var.public_path}/assets","*.{jpg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
-  source = "${var.assets_path}/${each.key}"
-  etag = filemd5("${var.assets_path}/${each.key}")
-
+  source = "${var.public_path}/assets/${each.key}"
+  etag = filemd5("${var.public_path}/assets/${each.key}")
   lifecycle {
-    replace_triggered_by = [ terraform_data.content_version.output ]
-    ignore_changes = [ etag ]
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
   }
 }
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath
+  source = "${var.public_path}/error.html"
   content_type = "text/html"
 
-  etag = filemd5(var.error_html_filepath)
-  lifecycle {
-    replace_triggered_by = [ terraform_data.content_version.output ]
-    ignore_changes = [ etag ]
-  }
+  etag = filemd5("${var.public_path}/error.html")
+  # lifecycle {
+  #   replace_triggered_by = [ terraform_data.content_version.output ]
+  #   ignore_changes = [ etag ]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -89,5 +88,3 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
 }
-
-
